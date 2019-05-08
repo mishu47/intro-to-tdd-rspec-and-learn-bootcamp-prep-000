3fe99cedbc3228b1d798dd9e035349e8fb6a9b0d
Mohammad Hossain <mohammadthossain@gmail.com>
2019-05-08 07:56:20 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -2,8 +2,12 @@ require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
-    age_of_person = current_age_for_birth_year(1984)
-
-    expect(age_of_person).to eq(19)https://learn.co/tracks/bootcamp-prep/ruby-fundamentals/methods/tdd-rspec-and-learn#
+    age_of_person =  Time.now.year
+    birth_year = 1984
+    answer = current_year - birth_year 
+    
+      age_of_person = current_age_for_birth_year(birth_year)
+      expect (age_of_person).to(answer)
+  
   end
 end
