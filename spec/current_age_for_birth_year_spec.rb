0a0de8d6e336f178f18c1465a76e8ebcf60f28a1
Mohammad Hossain <mohammadthossain@gmail.com>
2019-05-08 08:00:57 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -7,7 +7,7 @@ describe "current_age_for_birth_year method" do
     answer = current_year - birth_year 
     
       age_of_person = current_age_for_birth_year(birth_year)
-      expect (age_of_person).to(answer)
+      expect (age_of_person).to eq(answer)
   
   end
 end
